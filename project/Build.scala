fa99bb309fc473eb9b9b1dd40a12a8e7cf867cb7
Cory McIlroy <ubuntu@ip-10-32-105-211.ec2.internal>
2012-08-19 16:40:20 +0000

    oauth begin

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,16 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "redis.clients" % "jedis" % "2.0.0",
+      "com.force.api" % "force-rest-api" % "0.0.19",
+      "com.typesafe" %% "play-plugins-redis" % "2.0.2",
+      "org.sedis" %% "sedis" % "1.0.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
+     resolvers += "force-rest-api repository on GitHub" at "http://jesperfj.github.com/force-rest-api/repository/",
+     resolvers += "Sedis repository" at "http://guice-maven.googlecode.com/svn/trunk"
     )
 
 }
